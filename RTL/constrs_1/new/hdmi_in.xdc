f8b6f35db0fa9cca223cf42a7993fa8792f6077f
ZhongTianPan <1135820735@qq.com>
2023-10-07 10:17:26 +0800

    a version that could run the hdmi-ddr-hdmi demo (mismatch still exist)

---
+++ RTL/constrs_1/new/hdmi_in.xdc
@@ -0,0 +1,78 @@
+set_property PACKAGE_PIN AB11  [get_ports vin_clk]
+set_property PACKAGE_PIN Y14		 [get_ports {vin_data[0]}]
+set_property PACKAGE_PIN Y13		 [get_ports {vin_data[1]}]
+set_property PACKAGE_PIN AB13  [get_ports {vin_data[2]}]
+set_property PACKAGE_PIN AC12  [get_ports {vin_data[3]}]
+set_property PACKAGE_PIN AC14  [get_ports {vin_data[4]}]
+set_property PACKAGE_PIN AC13  [get_ports {vin_data[5]}]
+set_property PACKAGE_PIN AD12  [get_ports {vin_data[6]}]
+set_property PACKAGE_PIN AD11  [get_ports {vin_data[7]}]
+set_property PACKAGE_PIN AD15  [get_ports {vin_data[8]}]
+set_property PACKAGE_PIN AD14  [get_ports {vin_data[9]}]
+set_property PACKAGE_PIN AG13  [get_ports {vin_data[10]}]
+set_property PACKAGE_PIN AH13  [get_ports {vin_data[11]}]
+set_property PACKAGE_PIN AB9		  [get_ports {vin_data[12]}]
+set_property PACKAGE_PIN AB10  [get_ports {vin_data[13]}]
+set_property PACKAGE_PIN Y10		  [get_ports {vin_data[14]}]
+set_property PACKAGE_PIN W10		  [get_ports {vin_data[15]}]
+set_property PACKAGE_PIN W11		  [get_ports {vin_data[16]}]
+set_property PACKAGE_PIN W12		  [get_ports {vin_data[17]}]
+set_property PACKAGE_PIN W13		  [get_ports {vin_data[18]}]
+set_property PACKAGE_PIN W14		  [get_ports {vin_data[19]}]
+set_property PACKAGE_PIN AB15  [get_ports {vin_data[20]}]
+set_property PACKAGE_PIN AB14  [get_ports {vin_data[21]}]
+set_property PACKAGE_PIN AG11  [get_ports {vin_data[22]}]
+set_property PACKAGE_PIN AF11  [get_ports {vin_data[23]}]
+set_property PACKAGE_PIN AA13   [get_ports vin_de]
+set_property PACKAGE_PIN AA12   [get_ports vin_hs]
+set_property PACKAGE_PIN AD10   [get_ports vin_nreset]
+set_property PACKAGE_PIN Y12		   [get_ports vin_vs]
+
+
+set_property PACKAGE_PIN AF12 [get_ports vin_scl]
+set_property PACKAGE_PIN AE12 [get_ports vin_sda]
+
+set_property IOSTANDARD LVCMOS33 [get_ports vin_scl]
+set_property IOSTANDARD LVCMOS33 [get_ports vin_sda]
+
+
+set_property PULLUP true [get_ports vin_scl]
+set_property PULLUP true [get_ports vin_sda]
+
+
+#set_property PACKAGE_PIN F10   [get_ports edid_scl]
+#set_property PACKAGE_PIN G11   [get_ports edid_sda]
+set_property PACKAGE_PIN E8   [get_ports hpd]
+
+#set_property IOSTANDARD LVCMOS33 [get_ports edid_scl]
+#set_property IOSTANDARD LVCMOS33 [get_ports edid_sda]
+set_property IOSTANDARD LVCMOS18 [get_ports hpd]
+
+set_property IOSTANDARD LVCMOS33 [get_ports vin_clk]
+set_property IOSTANDARD LVCMOS33 [get_ports {vin_data[*]}]
+set_property IOSTANDARD LVCMOS33 [get_ports vin_de]
+set_property IOSTANDARD LVCMOS33 [get_ports vin_hs]
+set_property IOSTANDARD LVCMOS33 [get_ports vin_vs]
+set_property IOSTANDARD LVCMOS33 [get_ports vin_nreset]
+
+
+
+set_property IOB TRUE [get_ports {vin_data[*]}]
+set_property IOB TRUE [get_ports vin_de]
+set_property IOB TRUE [get_ports vin_hs]
+set_property IOB TRUE [get_ports vin_vs]
+
+set_property DRIVE 12 [get_ports {vin_data[*]}]
+set_property DRIVE 12 [get_ports vin_clk]
+set_property DRIVE 12 [get_ports vin_de]
+set_property DRIVE 12 [get_ports vin_hs]
+
+create_clock -period 6.734 -name vin_clk -waveform {0.000 3.367} [get_ports vin_clk]
+set_input_delay -clock [get_clocks vin_clk] -min -add_delay 2.500 [get_ports {vin_data[*]}]
+set_input_delay -clock [get_clocks vin_clk] -max -add_delay 3.500 [get_ports {vin_data[*]}]
+set_input_delay -clock [get_clocks vin_clk] -min -add_delay 2.500 [get_ports vin_de]
+set_input_delay -clock [get_clocks vin_clk] -max -add_delay 3.500 [get_ports vin_de]
+set_input_delay -clock [get_clocks vin_clk] -min -add_delay 2.500 [get_ports vin_hs]
+set_input_delay -clock [get_clocks vin_clk] -max -add_delay 3.500 [get_ports vin_hs]
+set_input_delay -clock [get_clocks vin_clk] -min -add_delay 2.500 [get_ports vin_vs]
+set_input_delay -clock [get_clocks vin_clk] -max -add_delay 3.500 [get_ports vin_vs]
--- RTL/sources_1/new/hdmi-loop/hdmi_loop.v
+++ RTL/sources_1/new/hdmi-loop/hdmi_loop.v
@@ -3,6 +3,17 @@ module hdmi_loop(
         input                   sys_clk_p
     ,   input                   sys_clk_n		
 
+	,   inout				    vin_scl
+	,   inout				    vin_sda
+
+	,   output 				    vin_nreset
+	,   input 				    vin_clk
+	,   input 				    vin_hs
+	,   input 				    vin_vs
+	,   input 				    vin_de
+	,   input[23:0] 		    vin_data
+	,   output                  hpd
+
     ,   inout                   vout_scl
     ,   inout                   vout_sda
 
@@ -13,10 +24,8 @@ module hdmi_loop(
 	,   output[23:0]            vout_data
 );
 
-wire[9:0]    adv7511_lut_index;
-wire[31:0]   adv7511_lut_data;
-
-
+//---------------------------------------------------
+//clock
 wire 		clk_100m;
 wire 		locked;
 
@@ -32,28 +41,101 @@ clk_wiz_0 u_clk_wiz_0
 	.locked(locked)
  );
 
-i2c_config i2c_config_m1(
-     .rst                        (~locked                   ),
-     .clk                        (clk_100m                  ),
-     .clk_div_cnt                (16'd499                   ),
-     .i2c_addr_2byte             (1'b0                      ),
-     .lut_index                  (adv7511_lut_index         ),
-     .lut_dev_addr               (adv7511_lut_data[31:24]   ),
-     .lut_reg_addr               (adv7511_lut_data[23:8]    ),
-     .lut_reg_data               (adv7511_lut_data[7:0]     ),
-     .error                      (                          ),
-     .done                       (                          ),
-     .i2c_scl                    (vout_scl                  ),
-     .i2c_sda                    (vout_sda                  )
- );
- //configure look-up table
- lut_adv7511 lut_adv7511_m0(
-     .lut_index                  (adv7511_lut_index                ),
-     .lut_data                   (adv7511_lut_data                 )
- ); 
+//---------------------------------------------------
+//hdmi out
+wire[9:0]    adv7511_lut_index;
+wire[31:0]   adv7511_lut_data;
 
+i2c_config i2c_config_m1(
+    .rst                        (~locked                   ),
+    .clk                        (clk_100m                  ),
+    .clk_div_cnt                (16'd499                   ),
+    .i2c_addr_2byte             (1'b0                      ),
+    .lut_index                  (adv7511_lut_index         ),
+    .lut_dev_addr               (adv7511_lut_data[31:24]   ),
+    .lut_reg_addr               (adv7511_lut_data[23:8]    ),
+    .lut_reg_data               (adv7511_lut_data[7:0]     ),
+    .error                      (                          ),
+    .done                       (                          ),
+    .i2c_scl                    (vout_scl                  ),
+    .i2c_sda                    (vout_sda                  )
+);
+
+lut_adv7511 lut_adv7511_m0(
+        .lut_index              (adv7511_lut_index          )
+    ,   .lut_data               (adv7511_lut_data           )
+); 
+
+ 
+//---------------------------------------------------
+//hdmi in
+wire[9:0]    adv7611_lut_index;
+wire[31:0]   adv7611_lut_data;
+
+reg 		vin_hs_d0;
+reg 		vin_vs_d0;
+reg 		vin_de_d0;
+reg[23:0] 	vin_data_d0;
+reg 		vin_hs_d1;
+reg 		vin_vs_d1;
+reg 		vin_de_d1;
+reg[23:0] 	vin_data_d1;
+reg 		vin_hs_d2;
+reg 		vin_vs_d2;
+reg 		vin_de_d2;
+reg[23:0] 	vin_data_d2;
+
+assign hpd = 1'b1;
+
+i2c_config i2c_config_m0(
+        .rst                (~locked                    )
+    ,   .clk                (clk_100m                   )
+    ,   .clk_div_cnt        (16'd499                    )
+    ,   .i2c_addr_2byte     (1'b0                       )
+    ,   .lut_index          (adv7611_lut_index          )
+    ,   .lut_dev_addr       (adv7611_lut_data[31:24]    )
+    ,   .lut_reg_addr       (adv7611_lut_data[23:8]     )
+    ,   .lut_reg_data       (adv7611_lut_data[7:0]      )
+    ,   .error              (                           )
+    ,   .done               (                           )
+    ,   .i2c_scl            (vin_scl                    )
+    ,   .i2c_sda            (vin_sda                    )
+);
+
+lut_adv7611 lut_adv7611_m0(
+        .lut_index          (adv7611_lut_index          )
+    ,   .lut_data           (adv7611_lut_data           )
+); 
+
+always@(posedge vin_clk) begin
+    vin_hs_d0   <= vin_hs;
+    vin_vs_d0   <= vin_vs;
+    vin_de_d0   <= vin_de;
+    vin_data_d0 <= vin_data;
+
+    vin_hs_d1   <= vin_hs_d0;
+    vin_vs_d1   <= vin_vs_d0;
+    vin_de_d1   <= vin_de_d0;
+    vin_data_d1 <= vin_data_d0; 
+    
+    vin_hs_d2   <= vin_hs_d1;
+    vin_vs_d2   <= vin_vs_d1;
+    vin_de_d2   <= vin_de_d1;
+    vin_data_d2 <= vin_data_d1;   
+end
+
+
+//---------------------------------------------------
+//system wrapper
 design_1_wrapper u_design_1_wrapper(
-        .video_clk_0        (vout_clk   )
+        .cmos_clk           (vin_clk    )
+    ,   .cmos_rst_n         (locked     )
+    ,   .cmos_vsync         (vin_vs_d2  )
+    ,   .cmos_href          (vin_hs_d2  )
+    ,   .cmos_de            (vin_de_d2  )
+    ,   .cmos_data          (vin_data_d2)
+
+    ,   .video_clk_0        (vin_clk    )
     ,   .video_vsync_0      (vout_vs    )
     ,   .video_href_0       (vout_hs    )
     ,   .video_de_0         (vout_de    )
